868677d62a2759323a1784517f409e3e6423d01f
Kartik Verma <katik2129.be21@chitkara.edu.in>
2023-12-12 21:14:02 +0530

    Removed seperate pages for routing service page

--- index.js
+++ index.js
@@ -12,74 +12,79 @@ app.set("views", path.join(__dirname, "/views"));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-    res.render("index", { categories: indexPageData["categories"] });
+  res.render("index", { categories: indexPageData["categories"] });
 });
 
 app.get("/blog", (req, res) => {
-    res.render("blog", { title: "Blogs" });
+  res.render("blog", { title: "Blogs" });
 });
 
 app.get("/team", (req, res) => {
-    res.render("team", { title: "Team" });
+  res.render("team", { title: "Team" });
 });
 
 app.get("/feedback", (req, res) => {
-    res.render("feedback_page", { title: "Feedback" });
+  res.render("feedback_page", { title: "Feedback" });
 });
 
 app.get("/contact", (req, res) => {
-    res.render("contact_page", { title: "Contact Us" });
+  res.render("contact_page", { title: "Contact Us" });
 });
 
 app.get("/signIn", (req, res) => {
-    res.render("sign_in_page", { title: "Sign In" });
+  res.render("sign_in_page", { title: "Sign In" });
 });
 
 app.get("/equipments/:service", (req, res) => {
-    const { service } = req.params;
-    switch (service) {
-        case "IA":
-            res.render("indianArmyEquipmentsPage", {
-                title: "Indian Army Equipments",
-            });
-            break;
-        case "IAF":
-            res.render("indianAirforceEquipments", {
-                title: "Indian Airforce Equipments",
-            });
-            break;
-        case "IN":
-            res.render("indianNavyEquipmentsPage", {
-                title: "Indian Navy Equipments",
-            });
-            break;
-        default:
-            res.render("404 - Page Not Found", {
-                title: "404 - Page Not Found",
-            });
-            break;
-    }
+  const { service } = req.params;
+  switch (service) {
+    case "IA":
+      res.render("servicePage", {
+        title: "Indian Army Equipments",
+        fulldata,
+        service,
+      });
+      break;
+    case "IAF":
+      res.render("servicePage", {
+        title: "Indian Airforce Equipments",
+        fulldata,
+        service,
+      });
+      break;
+    case "IN":
+      res.render("servicePage", {
+        title: "Indian Navy Equipments",
+        fulldata,
+        service,
+      });
+      break;
+    default:
+      res.render("404 - Page Not Found", {
+        title: "404 - Page Not Found",
+      });
+      break;
+  }
 });
 
 app.get("/equipments/:service/category/:category", (req, res) => {
-    const { service, category } = req.params;
-    res.render("categoryPage", {
-        title: category,
-        service,
-        fulldata,
-    });
+  const { service, category } = req.params;
+  res.render("categoryPage", {
+    title: category,
+    service,
+    fulldata,
+  });
 });
 
-//temp
-app.get("/equipments/:service/:equipment", (req, res) => {
-    const { service, equipment } = req.params;
-    res.render(equipment, { title: equipment });
+app.get("/equipments/:service/:category/:equipment", (req, res) => {
+  const { service, category, equipment } = req.params;
+  res.render("equipment", { service, category, title: equipment, fulldata });
 });
 
 app.get("*", (req, res) => {
-    res.render("404 - Page Not Found", { title: "404 - Page Not Found" });
+  res.render("404 - Page Not Found", { title: "404 - Page Not Found" });
 });
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
