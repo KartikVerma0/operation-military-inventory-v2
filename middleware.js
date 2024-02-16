63900881e2a0739f584b9a6e2fe0460a76dde961
Kartik Verma <katik2129.be21@chitkara.edu.in>
2024-02-16 23:33:22 +0530

    Initialised Middleware file

---
+++ middleware.js
@@ -0,0 +1,38 @@
+function isLoggedIn(req, res, next) {
+    if (!req.isAuthenticated()) {
+        req.session.returnTo = req.originalUrl;
+        req.flash("error", "You need to login first!");
+        return res.redirect("/signin");
+    }
+    next();
+}
+
+function storeReturnTo(req, res, next) {
+    res.locals.returnTo = req.session.returnTo;
+    next();
+}
+
+const { Equipment } = require("./modals/equipment");
+const DraftEquipment = require("./modals/draftEquipment");
+
+async function hasRights(req, res, next) {
+    const { id } = req.params;
+    const { view } = req.query;
+    let equipment = {};
+    if (view == "draft") {
+        equipment = await DraftEquipment.findById(id);
+    }
+    if (view == "saved") {
+        equipment = await Equipment.findById(id);
+    }
+    if (
+        equipment.user._id.toString() == req.user._id.toString() ||
+        req.user.role == "moderator" ||
+        req.user.role == "administrator"
+    )
+        return next();
+
+    res.status(403).send("Unauthorised access");
+}
+
+module.exports = { isLoggedIn, storeReturnTo, hasRights };
