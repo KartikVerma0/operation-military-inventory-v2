a299bbdf919c1646dae20899aa817292b9c0f6c9
Kartik Verma <katik2129.be21@chitkara.edu.in>
2024-02-16 23:33:06 +0530

    Added Authentication using passportJS

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ const authRouter = require("./routes/authRoute");
 const mongoose = require("mongoose");
 const flash = require("connect-flash");
 const methodOverride = require("method-override");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./modals/user");
 
 mongoose
     .connect(process.env.MONGO_URI)
@@ -25,10 +30,33 @@ mongoose
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
+const sess = {
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGO_URI,
+        ttl: 14 * 24 * 60 * 60,
+    }),
+};
+app.use(session(sess));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(flash());
 app.use(methodOverride("_method"));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+    next();
+});
 
 app.use("/", pageRouter);
 app.use("/", authRouter);
