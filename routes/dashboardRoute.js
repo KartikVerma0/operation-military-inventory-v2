fafe40b28b6c899b159855fa4d71f86655a6bc91
Kartik Verma <katik2129.be21@chitkara.edu.in>
2024-03-13 12:52:38 +0530

    Added logic for admin dashboard

--- routes/dashboardRoute.js
+++ routes/dashboardRoute.js
@@ -1,16 +1,23 @@
 const express = require("express");
 const router = express.Router();
 
-const { Equipment } = require("../models/equipment");
+const { Equipment, UnsavedEquipment } = require("../models/equipment");
 const DraftEquipment = require("../models/draftEquipment");
 
 const { isLoggedIn } = require("../middleware");
 
 router.get("/dashboard", isLoggedIn, async (req, res) => {
     const id = res.locals.currentUser._id;
+    if (res.locals.currentUser.role === "administrator") {
+        const draftEquipments = await DraftEquipment.find({ user: id });
+        const savedEquipments = await Equipment.find({ user: id });
+        const unsavedEquipments = await UnsavedEquipment.find({});
+        return res.render("adminDashboard", { draftEquipments, savedEquipments, unsavedEquipments });
+    }
     const draftEquipments = await DraftEquipment.find({ user: id });
     const savedEquipments = await Equipment.find({ user: id });
-    res.render("dashboard", { draftEquipments, savedEquipments });
+    const unsavedEquipments = await UnsavedEquipment.find({ user: id });
+    return res.render("dashboard", { draftEquipments, savedEquipments, unsavedEquipments });
 });
 
 module.exports = router;
