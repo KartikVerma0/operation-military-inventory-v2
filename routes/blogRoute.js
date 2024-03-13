e99efabda249cb5f6fdf233fdd9db7157974c1e3
Kartik Verma <katik2129.be21@chitkara.edu.in>
2024-03-13 20:42:46 +0530

    Removed some routes

--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -23,40 +23,40 @@ router.get("/", async (req, res) => {
     }
 });
 
-router.get("/new", isLoggedIn, (req, res) => {
-    res.render("newBlog");
-});
+// router.get("/new", isLoggedIn, (req, res) => {
+//     res.render("newBlog");
+// });
 
-router.post("/", isLoggedIn, async (req, res) => {
-    const { headline, articleURL, thumbnailURL } = req.body;
-    const newBlog = new Blog({
-        headline,
-        articleURL,
-        thumbnailURL,
-        publishedDate: Date.now(),
-        lastModifiedDate: Date.now(),
-        author: "Admin",
-    });
-    newBlog.markModified("publishedDate");
-    await newBlog.save();
-    // req.flash("success", "Successfully Published a new Blog");
-    res.redirect("/blog");
-});
+// router.post("/", isLoggedIn, async (req, res) => {
+//     const { headline, articleURL, thumbnailURL } = req.body;
+//     const newBlog = new Blog({
+//         headline,
+//         articleURL,
+//         thumbnailURL,
+//         publishedDate: Date.now(),
+//         lastModifiedDate: Date.now(),
+//         author: "Admin",
+//     });
+//     newBlog.markModified("publishedDate");
+//     await newBlog.save();
+//     // req.flash("success", "Successfully Published a new Blog");
+//     res.redirect("/blog");
+// });
 
-router.get("/edit/:id", isLoggedIn, async (req, res) => {
-    const { id } = req.params;
-    const blog = await Blog.findById(id);
-    res.render("editBlog", { blog });
-});
+// router.get("/edit/:id", isLoggedIn, async (req, res) => {
+//     const { id } = req.params;
+//     const blog = await Blog.findById(id);
+//     res.render("editBlog", { blog });
+// });
 
-router.put("/edit/:id", isLoggedIn, async (req, res) => {
-    const { id } = req.params;
-    const { headline, articleURL, thumbnailURL } = req.body;
-    const blog = await Blog.findByIdAndUpdate(
-        { _id: id },
-        { headline, articleURL, thumbnailURL, lastModifiedDate: Date.now() }
-    );
-    res.redirect("/blog");
-});
+// router.put("/edit/:id", isLoggedIn, async (req, res) => {
+//     const { id } = req.params;
+//     const { headline, articleURL, thumbnailURL } = req.body;
+//     const blog = await Blog.findByIdAndUpdate(
+//         { _id: id },
+//         { headline, articleURL, thumbnailURL, lastModifiedDate: Date.now() }
+//     );
+//     res.redirect("/blog");
+// });
 
 module.exports = router;
