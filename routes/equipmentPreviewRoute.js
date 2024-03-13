ebec966e8e3abb744cd2be2b3b44830e3f41761e
Kartik Verma <katik2129.be21@chitkara.edu.in>
2024-03-13 12:51:02 +0530

    New Equipment, Preview Equipment, Edit Equipment routes shifted to their respective files.

---
+++ routes/equipmentPreviewRoute.js
@@ -0,0 +1,150 @@
+const express = require("express");
+const router = express.Router();
+
+const { isLoggedIn, hasRights } = require("../middleware");
+
+const { Equipment, UnsavedEquipment } = require("../models/equipment");
+const DraftEquipment = require("../models/draftEquipment");
+
+router
+    .route("/:id")
+    .get(isLoggedIn, hasRights, async (req, res) => {
+        const { id } = req.params;
+        const { view } = req.query;
+        let equipment = {};
+        if (view == "draft") {
+            equipment = await DraftEquipment.findById(id);
+        }
+        if (view == "saved") {
+            equipment = await Equipment.findById(id);
+        }
+        if (view == "unsaved") {
+            equipment = await UnsavedEquipment.findById(id);
+        }
+
+        res.render("equipmentPreview", { equipment, view });
+    })
+    .post(isLoggedIn, hasRights, async (req, res) => {
+        const { id } = req.params;
+        const { view } = req.body;
+        let {
+            service,
+            category,
+            subCategory,
+            name,
+            description,
+            images,
+            icon,
+            videoLink,
+            learnMoreLink,
+            users,
+            hasDecomissioned
+        } = {};
+
+        if (view == "draft") {
+            const equipment = await DraftEquipment.findById(id);
+            service = equipment.service;
+            category = equipment.category;
+            subCategory = equipment.subCategory;
+            name = equipment.name;
+            description = equipment.description;
+            images = equipment.images;
+            icon = equipment.preview.icon;
+            videoLink = equipment.fullInfo.working.videoLink;
+            learnMoreLink = equipment.fullInfo.learnMoreLink;
+            users = equipment.fullInfo.users;
+            user = equipment.user;
+            hasDecomissioned = equipment.decomissionDetails.hasDecomissioned;
+
+            await new UnsavedEquipment({
+                service,
+                category,
+                subCategory,
+                name,
+                description,
+                images,
+                preview: {
+                    icon,
+                },
+                fullInfo: {
+                    learnMoreLink,
+                    users,
+                    working: {
+                        videoLink,
+                    },
+                },
+                user,
+                decomissionDetails: {
+                    hasDecomissioned
+                },
+            }).save();
+
+            await equipment.deleteOne();
+            req.flash("success", "Your New Equipment will soon be processed.");
+
+        }
+
+        if (view == "unsaved") {
+            if (res.locals.currentUser.role === "user") {
+                req.flash("error", "You are not authorized.")
+                return res.status(401).redirect("/dashboard");
+            } else if (res.locals.currentUser.role === "administrator" || res.locals.currentUser.role === "moderator") {
+                const equipment = await UnsavedEquipment.findById(id);
+                service = equipment.service;
+                category = equipment.category;
+                subCategory = equipment.subCategory;
+                name = equipment.name;
+                description = equipment.description;
+                images = equipment.images;
+                icon = equipment.preview.icon;
+                videoLink = equipment.fullInfo.working.videoLink;
+                learnMoreLink = equipment.fullInfo.learnMoreLink;
+                users = equipment.fullInfo.users;
+                user = equipment.user;
+                hasDecomissioned = equipment.decomissionDetails.hasDecomissioned;
+
+                await new Equipment({
+                    service,
+                    category,
+                    subCategory,
+                    name,
+                    description,
+                    images,
+                    preview: {
+                        icon,
+                    },
+                    fullInfo: {
+                        learnMoreLink,
+                        users,
+                        working: {
+                            videoLink,
+                        },
+                    },
+                    user,
+                    decomissionDetails: {
+                        hasDecomissioned
+                    },
+                }).save();
+
+                await equipment.deleteOne();
+                req.flash("success", "Your New Equipment is published.");
+            }
+
+        }
+
+        if (view == "saved") {
+            const equipment = await Equipment.findById(id);
+            service = equipment.service;
+            // category = equipment.category;
+            // name = equipment.name;
+            // description = equipment.description;
+            // images = equipment.images;
+            // icon = equipment.preview.icon;
+            // videoLink = equipment.fullInfo.working.videoLink;
+            // learnMoreLink = equipment.fullInfo.learnMoreLink;
+            // users = equipment.fullInfo.users;
+        }
+        res.redirect(`/equipments/${service}`);
+    });
+
+module.exports = router;
--- routes/equipmentRoute.js
+++ routes/equipmentRoute.js
@@ -1,291 +1,26 @@
 const express = require("express");
 const router = express.Router();
 const cloudinary = require("cloudinary").v2;
-const { CloudinaryStorage } = require("multer-storage-cloudinary");
-const multer = require("multer");
+
+const equipmentEditRoute = require("./equipmentEditRoute");
+const newEquipmentRoute = require("./newEquipmentRoute");
+const equipmentPreviewRoute = require("./equipmentPreviewRoute");
 
 const { isLoggedIn, hasRights } = require("../middleware");
 
-// Configure cloudinary instance with your cloudinary credentials
-cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_SECRET,
-});
-
-const storage = new CloudinaryStorage({
-    cloudinary: cloudinary,
-    params: {
-        folder: "operationMilitaryInventory",
-    },
-});
-
-const upload = multer({
-    storage: storage,
-    fileFilter: function (req, file, cb) {
-        if (file.fieldname == "equipmentIcon" && file.mimetype == "image/png") {
-            cb(null, true);
-            return;
-        } else if (
-            file.fieldname == "equipmentIcon" &&
-            file.mimetype != "image/png"
-        ) {
-            cb(
-                new Error("Only png files are allowed in Equipment Icon"),
-                false
-            );
-        }
-        cb(null, true);
-    },
-});
-
-const { Equipment } = require("../models/equipment");
+const { Equipment, UnsavedEquipment } = require("../models/equipment");
 const DraftEquipment = require("../models/draftEquipment");
 const {
-    serviceCategories,
-    subCategoriesArray,
+    serviceCategories, subCategoriesArray
 } = require("../public/js/categoryArrayForBackend");
 
 const fulldata = require("../public/json/data.json");
 
-router
-    .route("/new")
-    .get(isLoggedIn, (req, res) => {
-        res.render("newEquipmentForm");
-    })
-    .post(
-        isLoggedIn,
-        (req, res, next) => {
-            upload.fields([
-                { name: "equipmentIcon", maxCount: 1 },
-                { name: "images", maxCount: 10 },
-            ])(req, res, function (err) {
-                // Handle multer errors
-                if (err instanceof multer.MulterError) {
-                    // Delete uploaded files if any
-                    if (req.files) {
-                        req.files.forEach((file) => {
-                            fs.unlink(file.path, (err) => {
-                                if (err) {
-                                    console.error("Error deleting file:", err);
-                                }
-                            });
-                        });
-                    }
-                    return res.status(400).json({ error: err.message });
-                } else if (err) {
-                    return res.status(500).json({ error: "Server error" });
-                }
-                next();
-            });
-        },
-        async (req, res) => {
-            const {
-                name,
-                service,
-                category,
-                subCategory,
-                description,
-                users,
-                learnMoreLink,
-                videoLink,
-                hasDecomissioned,
-            } = req.body;
+router.use("/new", newEquipmentRoute);
 
-            const { equipmentIcon, images } = req.files;
+router.use("/preview", equipmentPreviewRoute);
 
-            const newEquipment = await new DraftEquipment({
-                service,
-                category,
-                subCategory,
-                name,
-                description,
-                images,
-                preview: {
-                    icon: equipmentIcon[0],
-                },
-                fullInfo: {
-                    learnMoreLink,
-                    users,
-                    working: {
-                        videoLink,
-                    },
-                },
-                decomissionDetails: {
-                    hasDecomissioned,
-                },
-                user: req.user,
-            }).save();
-
-            res.redirect(`/equipments/preview/${newEquipment._id}?view=draft`);
-        }
-    );
-
-router
-    .route("/preview/:id")
-    .get(isLoggedIn, hasRights, async (req, res) => {
-        const { id } = req.params;
-        const { view } = req.query;
-        let equipment = {};
-        if (view == "draft") {
-            equipment = await DraftEquipment.findById(id);
-        }
-        if (view == "saved") {
-            equipment = await Equipment.findById(id);
-        }
-
-        res.render("equipmentPreview", { equipment, view });
-    })
-    .post(isLoggedIn, hasRights, async (req, res) => {
-        const { id } = req.params;
-        const { view } = req.body;
-        let {
-            service,
-            category,
-            name,
-            description,
-            images,
-            icon,
-            videoLink,
-            learnMoreLink,
-            users,
-        } = {};
-
-        if (view == "draft") {
-            const equipment = await DraftEquipment.findById(id);
-            service = equipment.service;
-            category = equipment.category;
-            name = equipment.name;
-            description = equipment.description;
-            images = equipment.images;
-            icon = equipment.preview.icon;
-            videoLink = equipment.fullInfo.working.videoLink;
-            learnMoreLink = equipment.fullInfo.learnMoreLink;
-            users = equipment.fullInfo.users;
-            user = equipment.user;
-
-            await new Equipment({
-                service,
-                category,
-                name,
-                description,
-                images,
-                preview: {
-                    icon,
-                },
-                fullInfo: {
-                    learnMoreLink,
-                    users,
-                    working: {
-                        videoLink,
-                    },
-                },
-                user,
-            }).save();
-
-            await equipment.deleteOne();
-        }
-
-        if (view == "saved") {
-            const equipment = await Equipment.findById(id);
-            service = equipment.service;
-            // category = equipment.category;
-            // name = equipment.name;
-            // description = equipment.description;
-            // images = equipment.images;
-            // icon = equipment.preview.icon;
-            // videoLink = equipment.fullInfo.working.videoLink;
-            // learnMoreLink = equipment.fullInfo.learnMoreLink;
-            // users = equipment.fullInfo.users;
-        }
-        res.redirect(`/equipments/${service}`);
-    });
-
-router
-    .route("/edit/:id")
-    .get(isLoggedIn, hasRights, async (req, res) => {
-        const { id } = req.params;
-        const { view } = req.query;
-        let equipment = {};
-        if (view == "draft") {
-            equipment = await DraftEquipment.findById(id);
-        }
-        if (view == "saved") {
-            equipment = await Equipment.findById(id);
-        }
-        res.render("equipmentEditForm", { equipment, view });
-    })
-    .put(
-        isLoggedIn,
-        hasRights,
-        upload.fields([
-            { name: "equipmentIcon", maxCount: 1 },
-            { name: "images", maxCount: 10 },
-        ]),
-        async (req, res) => {
-            const { id } = req.params;
-            const {
-                view,
-                name,
-                service,
-                category,
-                subCategory,
-                description,
-                users,
-                learnMoreLink,
-                videoLink,
-                hasDecomissioned,
-            } = req.body;
-
-            const { equipmentIcon, images } = req.files;
-
-            let newEquipment = {};
-            if (view == "draft") {
-                const equipment = await DraftEquipment.findById(id);
-                equipment.name = name;
-                equipment.service = service;
-                equipment.category = category;
-                if (equipment.subCategory) equipment.subCategory = subCategory;
-                equipment.description = description;
-                equipment.fullInfo.learnMoreLink = learnMoreLink;
-                equipment.fullInfo.users = users;
-                equipment.fullInfo.working.videoLink = videoLink;
-                equipment.decomissionDetails.hasDecomissioned =
-                    hasDecomissioned;
-                if (equipmentIcon) {
-                    equipment.preview.icon = equipmentIcon[0];
-                }
-                if (images) {
-                    equipment.images.push(...images);
-                }
-
-                newEquipment = await equipment.save();
-            } else if (view == "saved") {
-                const equipment = await Equipment.findById(id);
-                equipment.name = name;
-                equipment.service = service;
-                equipment.category = category;
-                if (equipment.subCategory) equipment.subCategory = subCategory;
-                equipment.description = description;
-                equipment.fullInfo.learnMoreLink = learnMoreLink;
-                equipment.fullInfo.users = users;
-                equipment.fullInfo.working.videoLink = videoLink;
-                equipment.decomissionDetails.hasDecomissioned =
-                    hasDecomissioned;
-                if (equipmentIcon) {
-                    equipment.preview.icon = equipmentIcon[0];
-                }
-                if (images) {
-                    equipment.images.push(...images);
-                }
-
-                newEquipment = await equipment.save();
-            }
-            res.redirect(
-                `/equipments/preview/${newEquipment._id}?view=${view}`
-            );
-        }
-    );
+router.use("/edit", equipmentEditRoute);
 
 router.get("/IA", async (req, res) => {
     const equipments = await Equipment.find({ service: "IA" });
@@ -313,20 +48,30 @@ router.get("/IAF", async (req, res) => {
 });
 
 //update required
-router.get("/IN", (req, res) => {
+router.get("/IN", async (req, res) => {
+    const equipments = await Equipment.find({ service: "IN" });
     const renderInfoObject = {
         title: "Indian Navy Equipments",
+        equipments,
+        serviceCategories,
+        subCategoriesArray,
         fulldata,
         service: "IN",
     };
     res.render("indianNavyEquipmentsPage", renderInfoObject);
 });
 
+router.get("/unsaved/:id", async (req, res) => {
+    const { id } = req.params;
+    const equipment = await UnsavedEquipment.findById(id);
+    await equipment.populate("user");
+    res.render("equipment", { equipment, view: "unsaved" });
+});
 router.get("/:id", async (req, res) => {
     const { id } = req.params;
     const equipment = await Equipment.findById(id);
     await equipment.populate("user");
-    res.render("equipment", equipment);
+    res.render("equipment", { equipment, view: "saved" });
 });
 
 router.get("/:service/category/:category", async (req, res) => {
@@ -442,6 +187,26 @@ router.delete("/delete/:id", isLoggedIn, hasRights, async (req, res) => {
 
         await equipment.deleteOne();
     }
+    if (view == "unsaved") {
+        const equipment = await UnsavedEquipment.findById(id);
+        const images = equipment.images;
+        const icon = equipment.preview.icon;
+
+        cloudinary.api.delete_resources([icon.filename], {
+            type: "upload",
+            resource_type: "image",
+        });
+
+        for (let i = 0; i < images.length; i++) {
+            cloudinary.api.delete_resources([images[i].filename], {
+                type: "upload",
+                resource_type: "image",
+            });
+        }
+
+        await equipment.deleteOne();
+    }
+    req.flash("success", "Successfully Deleted Equipment")
     res.redirect(`/equipments/${service}`);
 });
 
